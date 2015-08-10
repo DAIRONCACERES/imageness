42674d271498376391dea37df43c57405f1fd491
DAIRONCACERES <dairon.caceres07@gmail.com>
2015-08-10 14:34:32 -0500

    imagenes

---
+++ imggaleria.js
@@ -0,0 +1,23 @@
+function cambio1(){
+	document.getElementById('imgprincipal').src='./imagenes/images1.jpg';
+}
+
+function cambio2(){
+	document.getElementById('imgprincipal').src='./imagenes/images2.jpg';
+}
+
+function cambio3(){
+	document.getElementById('imgprincipal').src='./imagenes/images3.jpg';
+}
+
+function cambio4(){
+	document.getElementById('imgprincipal').src='./imagenes/images4.jpg';
+}
+
+function cambio5(){
+	document.getElementById('imgprincipal').src='./imagenes/images5.jpg';
+}
+
+function volver(){
+	document.getElementById('imgprincipal').src='./imagenes/images1.jpg';
+}
